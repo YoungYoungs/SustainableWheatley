abba8c1e72e97374ead120f8c5b55ed9a71b0c0a
YoungYoungs <YoungYoungs@users.noreply.github.com>
2019-10-08 21:02:16 +0100

    Date correction

--- js/common.js
+++ js/common.js
@@ -1,17 +1,24 @@
 $(function() {
+
     var dates = [
-      {date:new Date(2019, 9, 12), label:"October 12<sup>th</sup>"},
-      {date:new Date(2019, 6, 20), label:"July 20<sup>th</sup>"},
-      {date:new Date(2019, 3, 27), label:"April 27<sup>th</sup>"},
-      {date:new Date(2019, 0, 26), label:"January 26<sup>th</sup>"}
+      {date:new Date(2020, 9, 10), label:"October 10<sup>th</sup>"},
+      {date:new Date(2020, 6, 18), label:"July 18<sup>th</sup>"},
+      {date:new Date(2020, 3, 18), label:"April 18<sup>th</sup>"},
+      {date:new Date(2020, 0, 25), label:"January 25<sup>th</sup>"},
+      {date:new Date(2019, 9, 12), label:"October 12<sup>th</sup>"}
     ]
+
     var label = "";
-    var now = Date.now();
+    var now = new Date();
+    now.setHours(0,0,0,0);
     var element = $("#NextSwapShopDate");
 
     for(i=0; i<dates.length; i++){
-      if(dates[i].date >= now){
+      if(dates[i].date > now){
         label = dates[i].label;
+      }
+      if(dates[i].date.getTime() === now.getTime()){
+        label = 'Today!'
       }
     }
 

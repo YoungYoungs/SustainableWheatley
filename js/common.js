c4788bc35433b5d86c980e1e7f44de7cacfcadbd
YoungYoungs <YoungYoungs@users.noreply.github.com>
2019-10-08 20:33:31 +0100

    Date correction

--- js/common.js
+++ js/common.js
@@ -1,6 +1,6 @@
 $(function() {
     var dates = [
-      {date:new Date(2019, 8, 12), label:"October 12<sup>th</sup>"},
+      {date:new Date(2019, 9, 12), label:"October 12<sup>th</sup>"},
       {date:new Date(2019, 6, 20), label:"July 20<sup>th</sup>"},
       {date:new Date(2019, 3, 27), label:"April 27<sup>th</sup>"},
       {date:new Date(2019, 0, 26), label:"January 26<sup>th</sup>"}
